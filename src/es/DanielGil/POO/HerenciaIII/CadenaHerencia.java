cef6d7423786a13ef08cfff3fe034425665f66ec
Celot1979 <zatak631979@gmail.com>
2021-02-04 12:52:41 +0100

    Cadena de herencia 3 FEb 2021

---
+++ src/es/DanielGil/POO/HerenciaIII/CadenaHerencia.java
@@ -0,0 +1,45 @@
+package es.DanielGil.POO.HerenciaIII;
+
+public class CadenaHerencia {
+
+	public static void main(String[] args) {
+		/* En este ejemplo veremos:
+		 * Cadena de Herencia.
+		 * Clase object.
+		 * Jerarquia de herencia.
+		 */
+		Clase4 miobj = new Clase4();
+		
+	
+	}
+
+}
+
+class Clase1{
+	public void metodo1() {
+		
+	}
+	
+}
+
+class Clase2 extends Clase1{
+	public void metodo2() {
+		
+	}
+	
+}
+
+class Clase3 extends Clase2{
+     public void metodo3() {
+		
+	}
+	
+}
+
+class Clase4 extends Clase3{
+      public void metodo4() {
+		
+	}
+	
+	
+}
