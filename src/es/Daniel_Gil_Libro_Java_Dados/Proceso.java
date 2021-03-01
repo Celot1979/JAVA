d0952c7bd86b6045e277b5d59a14744ba90367b7
Celot1979 <zatak631979@gmail.com>
2021-03-01 12:45:45 +0100

    Juego Dados (1 Marzo 2021)

--- src/es/Daniel_Gil_Libro_Java_Dados/Proceso.java
+++ src/es/Daniel_Gil_Libro_Java_Dados/Proceso.java
@@ -1,5 +1,78 @@
 package es.Daniel_Gil_Libro_Java_Dados;
 
+import java.util.Scanner;
+
 public class Proceso {
+	
+	public Proceso() {
+		Scanner d = new Scanner(System.in);
+		System.out.println("Cuanto dinero quieres depositar: ");
+		deposito = d.nextInt();
+		
+	}
+	
+	
+	public String getDeposito() {
+		return "El jugador ha depositado: " + deposito;
+	}
+	
+	public void setApuesta() {
+		Scanner a = new Scanner(System.in);
+		System.out.println("Cuanto dinero quieres depositar: ");
+		apuesta = a.nextInt();
+		
+		if (apuesta > deposito) {
+			System.out.println("No se admite esa apuesta por falta de fondos"); 
+		}
+		
+		
+	}
+	
+	
+	public void setTiradas() {
+		Dado1 = (int)(Math.random()*6+1);
+		Dado2 = (int)(Math.random()*6+1);
+		Dado3 = (int)(Math.random()*6+1);
+	}
+	
+	
+	public String getTiradas() {
+		return "Los dados ha dado como resultado: " + Dado1 + " "+ Dado2 + " "+ Dado3;
+	}
+	
+	
+	public void setJugadas() {
+		if(Dado1 == 6 && Dado2 ==6 && Dado3 == 6) {
+			deposito = deposito + (apuesta * 5);
+		}else if (Dado1 == Dado2 && Dado2 == Dado3) {
+			deposito = deposito + (apuesta * 3);
+		}else if(Dado1 == Dado2 && Dado2 != Dado3) {
+			deposito = deposito + (apuesta * 2);
+		}else if(Dado1 == Dado3 && Dado1 != Dado2) {
+			deposito = deposito + (apuesta * 2);
+		}else if (Dado1 != Dado2 && Dado1 != Dado3) {
+			deposito = deposito - apuesta;
+		}else if (Dado2 != Dado3 && Dado2 != Dado3) {
+			deposito = deposito - apuesta;
+		}
+	}
+	
+	
+	public String getJugadas() {
+		return "El jugador tiene en el deposito: " + deposito;
+	}
+	
+	
+	
+	
+	
+	
+	
+	private int deposito;
+	private int apuesta;
+	private int Dado1;
+	private int Dado2;
+	private int Dado3;
+	private int opcion;
 
 }
--- src/es/Daniel_Gil_Libro_Java_Dados/Tiradas.java
+++ src/es/Daniel_Gil_Libro_Java_Dados/Tiradas.java
@@ -1,10 +1,45 @@
 package es.Daniel_Gil_Libro_Java_Dados;
 
+import java.util.Scanner;
+
 public class Tiradas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Proceso j1 = new Proceso();
+		System.out.println(j1.getDeposito());
+		
+		j1.setApuesta();
+		
+		j1.setTiradas();
+		System.out.println(j1.getTiradas());
+		j1.setJugadas();
+		System.out.println(j1.getJugadas());
+		
+		int opcion = 1;
+		
+		while (opcion ==1) {
+			Scanner op1 = new Scanner(System.in);
+			System.out.println("Deseas Seguir jugando: ");
+			int opciones = op1.nextInt();
+			
+			if(opciones == 1) {
+				j1.setApuesta();
+				j1.setTiradas();
+				System.out.println(j1.getTiradas());
+				j1.setJugadas();
+				System.out.println(j1.getJugadas());
+			}else if (opciones == 2) {
+				System.out.println("Gracias por tu visita. Esparamos volver a verte pronto.");
+				opcion = 2;
+				
+			}
+			
+		}
+		
+	
+		
 	}
 
 }
